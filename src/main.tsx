432892bfa058f62d9dbfc98d5b3e5ada2f0df32e
Nahuel <nahueltorrezg@gmail.com>
2024-09-12 21:02:16 -0400

    Add folders to clean architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
-import Tailwind from 'primereact/passthrough/tailwind';
+import Tailwind from 'primereact/passthrough/tailwind'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<PrimeReactProvider value={{ unstyled: true, pt: Tailwind}}>
+		<PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
 			<App />
 		</PrimeReactProvider>
 	</StrictMode>
