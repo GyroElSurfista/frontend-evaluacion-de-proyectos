b029a55c87c8b2c774fb606682be88993058d979
Mariana Vallejos <marivalleandrea@gmail.com>
2024-09-24 08:44:26 -0400

    format

--- src/pages/ObjectivePage/Components/ObjectiveAccordion/ObjectiveAccordion.tsx
+++ src/pages/ObjectivePage/Components/ObjectiveAccordion/ObjectiveAccordion.tsx
@@ -1,4 +1,3 @@
-
 import './ObjectiveAccordion.css'
 import { useState } from 'react'
 interface Objective {
@@ -9,8 +8,8 @@ interface Objective {
 }
 
 interface ObjectiveAccordionProps {
-	objective: Objective;
-	indexObj: number;
+	objective: Objective
+	indexObj: number
 }
 
 const formatDate = (dateString: string) => {
@@ -31,14 +30,14 @@ const ObjectiveAccordion: React.FC<ObjectiveAccordionProps> = ({ objective, inde
 			<div className="hover:bg-[#c6caff] w-full border rounded border-[#c6caff] p-4" onClick={toggleAccordion}>
 				<div className="flex flex-row w-full justify-between items-center">
 					<div className="w-auto border-r-2 pr-2 border-[#c6caff]">
-						<span className='text-center text-[#1c1c1c] text-xl font-semibold'>Objetivo {indexObj}</span>
+						<span className="text-center text-[#1c1c1c] text-xl font-semibold">Objetivo {indexObj}</span>
 					</div>
 					<div className="w-9/12 text-start">
 						<span className="bg-red-200 rounded-xl text-sm p-1 text-gray-600 font-normal">{formatDate(objective.iniDate)}</span> -
 						<span className="bg-indigo-200 rounded-xl text-sm p-1 ml-1 text-gray-600 font-normal">{formatDate(objective.finDate)}</span>
 					</div>
 					<div className="w-auto pl-3 flex justify-items-end border-l-2 border-[#c6caff]">
-						<button className='px-2.5 py-[5px] bg-[#251b4d] rounded-lg shadow text-white text-base font-normal'>Ver entregable</button>
+						<button className="px-2.5 py-[5px] bg-[#251b4d] rounded-lg shadow text-white text-base font-normal">Ver entregable</button>
 					</div>
 				</div>
 			</div>
