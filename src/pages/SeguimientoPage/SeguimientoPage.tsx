ab2842215f415d2c65ef270bb70f7dcb364bb0a3
Mariana Vallejos <marivalleandrea@gmail.com>
2024-09-28 13:02:04 -0400

    Add outlet in SeguimientoPage to see a tracking sheet

--- src/pages/SeguimientoPage/SeguimientoPage.tsx
+++ src/pages/SeguimientoPage/SeguimientoPage.tsx
@@ -1,6 +1,6 @@
 import { Snackbar, SnackbarCloseReason, SnackbarContent } from '@mui/material'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 interface Objetivo {
   id: number
@@ -16,6 +16,8 @@ const SeguimientoPage = () => {
     { id: 2, nombre: 'Objetivo 2', planillaGenerada: false, planillaUrl: null },
     { id: 3, nombre: 'Objetivo 3', planillaGenerada: false, planillaUrl: null },
   ])
+
+  const location = useLocation()
 
   const [openSnackbar, setOpenSnackbar] = useState(false)
   const [snackbarMessage, setSnackbarMessage] = useState('')
@@ -43,48 +45,53 @@ const SeguimientoPage = () => {
     <div>
       <h2 className="text-black text-3xl font-semibold pl-6">Seguimiento</h2>
       <hr className="border-[1.5px] border-[#c6caff] mt-3 mb-6" />
+      {location.pathname === '/seguimiento' ? (
+        <>
+          {objetivos.map((objetivo) => (
+            <div key={objetivo.id} className="w-full h-[58px] pr-2.5 mb-3 bg-[#e0e3ff] rounded justify-between items-center inline-flex">
+              <div className="px-2.5 py-[15px] border-r border-[#c6caff] justify-center items-center gap-2.5 flex">
+                <div className="text-center text-[#1c1c1c] text-lg font-semibold">{objetivo.nombre}</div>
+              </div>
+              <div className="px-2.5 py-[5px] justify-center items-center gap-2.5 flex">
+                {!objetivo.planillaGenerada ? (
+                  <button className="button-primary" onClick={() => handleClick(objetivo.id)}>
+                    Generar Planilla
+                  </button>
+                ) : (
+                  <Link to={`/seguimiento/objetivo/${objetivo.id}`} className="button-primary">
+                    Ver Planilla
+                  </Link>
+                )}
+              </div>
+            </div>
+          ))}
 
-      {objetivos.map((objetivo) => (
-        <div key={objetivo.id} className="w-full h-[58px] pr-2.5 mb-3 bg-[#e0e3ff] rounded justify-between items-center inline-flex">
-          <div className="px-2.5 py-[15px] border-r border-[#c6caff] justify-center items-center gap-2.5 flex">
-            <div className="text-center text-[#1c1c1c] text-lg font-semibold">{objetivo.nombre}</div>
-          </div>
-          <div className="px-2.5 py-[5px] justify-center items-center gap-2.5 flex">
-            {!objetivo.planillaGenerada ? (
-              <button className="button-primary" onClick={() => handleClick(objetivo.id)}>
-                Generar Planilla
-              </button>
-            ) : (
-              <Link to={objetivo.planillaUrl || '#'} className="button-primary">
-                Ver Planilla
-              </Link>
-            )}
-          </div>
-        </div>
-      ))}
-
-      {/* Snackbar para mostrar los mensajes de éxito o error */}
-      <Snackbar
-        open={openSnackbar}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        autoHideDuration={5000}
-        onClose={handleCloseSnackbar}
-      >
-        <SnackbarContent
-          style={{
-            display: 'flex',
-            width: '325px',
-            padding: '15px 20px',
-            justifyContent: 'start',
-            alignItems: 'center',
-            gap: '10px',
-            borderRadius: '10px',
-            background: snackbarColor,
-            color: snackbarColor === '#D3FFD2' ? '#00A407' : '#A40000', // Cambia el color del texto basado en el tipo de mensaje
-          }}
-          message={snackbarMessage}
-        />
-      </Snackbar>
+          {/* Snackbar para mostrar los mensajes de éxito o error */}
+          <Snackbar
+            open={openSnackbar}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            autoHideDuration={5000}
+            onClose={handleCloseSnackbar}
+          >
+            <SnackbarContent
+              style={{
+                display: 'flex',
+                width: '325px',
+                padding: '15px 20px',
+                justifyContent: 'start',
+                alignItems: 'center',
+                gap: '10px',
+                borderRadius: '10px',
+                background: snackbarColor,
+                color: snackbarColor === '#D3FFD2' ? '#00A407' : '#A40000', // Cambia el color del texto basado en el tipo de mensaje
+              }}
+              message={snackbarMessage}
+            />
+          </Snackbar>
+        </>
+      ) : (
+        <Outlet />
+      )}
     </div>
   )
 }
--- src/pages/SeguimientoPage/TrackingSheet/TrackingSheet.tsx
+++ src/pages/SeguimientoPage/TrackingSheet/TrackingSheet.tsx
@@ -1,5 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const TrackingSheet = () => {
-  return <div>TrackingSheet</div>
-}
+  const { idObjetivo } = useParams();
+
+  return (
+    <div>
+      <h1>Detalles del Objetivo</h1>
+      <p>ID del objetivo: {idObjetivo}</p>
+    </div>
+  );}
 
 export default TrackingSheet
